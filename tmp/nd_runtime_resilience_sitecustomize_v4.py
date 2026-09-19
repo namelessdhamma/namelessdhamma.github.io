1caec3b8073093105d13f1545064c3f2e339d20e
namelessdhamma <namelessdhamma@gmail.com>
2026-09-19 09:41:41 +0300

    Add VK resilience runtime shim

---
+++ tmp/nd_runtime_resilience_sitecustomize_v4.py
@@ -0,0 +1,29 @@
+import urllib.request
+
+_OLD_LINEAR = "https://raw.githubusercontent.com/namelessdhamma/namelessdhamma.github.io/4861bf8228e4e4f19f7db98ef1970c4f41338cb6/tmp/nd_gateway_linear_bridge_v1.py"
+_NEW_LINEAR = "https://raw.githubusercontent.com/namelessdhamma/namelessdhamma.github.io/a4c560c6137e3d7038702592e8f2db71ecade787/tmp/nd_gateway_linear_bridge_v2_clean_compat.py"
+
+_OLD_V15B = "https://raw.githubusercontent.com/namelessdhamma/namelessdhamma.github.io/98529cbb27ee130b3087e93d5322d613ddf0095f/tmp/nd_vk_gateway_v15b_father_min_profile.py"
+_NEW_V16 = "https://raw.githubusercontent.com/namelessdhamma/namelessdhamma.github.io/6256fb1037d9deeb1601e9e0ca9ba8dd5e0975e5/tmp/nd_vk_gateway_v16_resilience_memory.py"
+
+_original_urlopen = urllib.request.urlopen
+_original_urlretrieve = urllib.request.urlretrieve
+
+def _swap_url(value):
+    if isinstance(value, str):
+        if value == _OLD_LINEAR:
+            return _NEW_LINEAR
+        if value == _OLD_V15B:
+            return _NEW_V16
+    return value
+
+def _urlopen(value, *args, **kwargs):
+    return _original_urlopen(_swap_url(value), *args, **kwargs)
+
+def _urlretrieve(value, *args, **kwargs):
+    return _original_urlretrieve(_swap_url(value), *args, **kwargs)
+
+urllib.request.urlopen = _urlopen
+urllib.request.urlretrieve = _urlretrieve
+
+print("ND_RUNTIME_RESILIENCE_SHIM_V4_ACTIVE", flush=True)
